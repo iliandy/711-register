0ae2acc70a4274a32ad82bda2878c0cc75a81e0e
Andy Li <iandyli@gmail.com>
2019-04-05 16:16:44 -0500

    Testing Formik without Bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,85 +1,53 @@
-import React, { Component } from "react"
-import { Formik } from "formik"
-import * as yup from "yup"
-import Button from "react-bootstrap/Button"
-import Form from "react-bootstrap/Form"
-class App extends Component {
-  schema = yup.object({
-    firstName: yup.string().required(),
-    lastName: yup.string().required(),
-    email: yup.string().required(),
-    password: yup.string().required(),
-    confirmPassword: yup.string().required(),
-    birthMon: yup.string().required(),
-    birthDay: yup.number().required(),
-    birthYear: yup.number().required(),
-    phone: yup.number(),
-    country: yup.bool().required(),
-    zip: yup.number().required(),
-    terms: yup.bool().required(),
-    offers: yup.bool(),
-  })
+import React from "react"
+import { Formik, Form, Field } from "formik"
+import * as Yup from "yup"
 
-  initialValues = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    birthMon: "",
-    birthDay: "",
-    birthYear: "",
-    phone: "",
-    country: false,
-    zip: "",
-    terms: false,
-    offers: false,
-  }
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  lastName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string()
+    .email("Invalid email")
+    .required("Required"),
+})
 
-  render() {
-    return (
-      <div>
-        <Formik
-          initialValues={this.initialValues}
-          validationSchema={this.schema}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2))
-              setSubmitting(false)
-            }, 400)
-          }}
-        >
-          {({
-            handleSubmit,
-            handleChange,
-            handleBlur,
-            values,
-            touched,
-            isValid,
-            errors,
-          }) => (
-            <Form onSubmit={handleSubmit}>
-              <Form.Group controlId="validationFormikFirstName">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="firstName"
-                  value={values.firstName}
-                  onChange={handleChange}
-                  isValid={touched.firstName && !errors.firstName}
-                  placeholder="Thor"
-                />
-                <Form.Control.Feedback type="invalid">
-                  Looks bad!
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Button type="submit">Register</Button>
-            </Form>
-          )}
-        </Formik>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div>
+    <h1>Signup</h1>
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={(values) => {
+        // same shape as initial values
+        console.log(values)
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <Field name="firstName" />
+          {errors.firstName && touched.firstName ? (
+            <div>{errors.firstName}</div>
+          ) : null}
+          <Field name="lastName" />
+          {errors.lastName && touched.lastName ? (
+            <div>{errors.lastName}</div>
+          ) : null}
+          <Field name="email" type="email" />
+          {errors.email && touched.email ? <div>{errors.email}</div> : null}
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+)
 
 export default App
